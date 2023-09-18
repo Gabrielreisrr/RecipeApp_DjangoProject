d524ef6436df29e8fc37a03ac5bc0118b51b084e
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2023-09-18 18:37:56 -0300

    add templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
+# A função render tem como objetivo renderizar um arquivo html dentro de uma função
 from django.shortcuts import render
+
+
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('contato')
